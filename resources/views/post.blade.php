9f39ee54969f6ff6afc80e102ef78e8cd47acee3
akbar <akbridha@gmail.com>
2022-09-06 14:01:57 +0800

    coba clone dari pc,view tambah tombol kembali dan

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.beranda')
 
 @section('container')
-<h3>lasjdlfja</h3>
+<h3>Post Focus</h3>
 <article>
 
     <h5>{{ $body["author"] }}</h5>
     <p>{{ $body["body"] }}</p>
+    <a href="/tahun">Kembali</a>
   </article>   
 @endsection
